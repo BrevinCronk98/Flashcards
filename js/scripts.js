769d9d571305d47cfb93c7273f89a575f5fadb2d
brevincronk and jackdunning <jackdunning@>
2020-01-14 14:41:48 -0800

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,137 @@
+$(document).ready(function() {
+  $(".js").click(function() {
+
+    $("#jsTitle").slideToggle();
+    $("#jsText").slideToggle();
+  });
+
+  $(".op").click(function() {
+
+    $("#opTitle").slideToggle();
+    $("#opText").slideToggle();
+  });
+
+  $(".var").click(function() {
+
+    $("#varTitle").slideToggle();
+    $("#varText").slideToggle();
+  });
+
+  $(".vnc").click(function() {
+
+    $("#vncTitle").slideToggle();
+    $("#vncText").slideToggle();
+  });
+
+  $(".func").click(function() {
+
+    $("#funcTitle").slideToggle();
+    $("#funcText").slideToggle();
+  });
+
+  $(".meth").click(function() {
+
+    $("#methTitle").slideToggle();
+    $("#methText").slideToggle();
+  });
+
+  $(".arg").click(function() {
+
+    $("#argitle").slideToggle();
+    $("#argText").slideToggle();
+  });
+
+  $(".param").click(function() {
+
+    $("#paramTitle").slideToggle();
+    $("#paramText").slideToggle();
+  });
+
+  $(".ret").click(function() {
+
+    $("#retTitle").slideToggle();
+    $("#retText").slideToggle();
+  });
+
+  $(".cm").click(function() {
+
+    $("#cmTitle").slideToggle();
+    $("#cmText").slideToggle();
+  });
+
+  $(".str").click(function() {
+
+    $("#strTitle").slideToggle();
+    $("#strText").slideToggle();
+  });
+
+  $(".bool").click(function() {
+
+    $("#boolTitle").slideToggle();
+    $("#boolText").slideToggle();
+  });
+
+  $(".und").click(function() {
+
+    $("#undTitle").slideToggle();
+    $("#undText").slideToggle();
+  });
+
+  $(".nan").click(function() {
+
+    $("#nanTitle").slideToggle();
+    $("#nanText").slideToggle();
+  });
+
+  $(".esc").click(function() {
+
+    $("#escTitle").slideToggle();
+    $("#escText").slideToggle();
+  });
+
+  $(".ale").click(function() {
+
+    $("#aleTitle").slideToggle();
+    $("#aleText").slideToggle();
+  });
+
+  $(".com").click(function() {
+
+    $("#comTitle").slideToggle();
+    $("#comText").slideToggle();
+  });
+
+  $(".jq").click(function() {
+
+    $("#jqTitle").slideToggle();
+    $("#jqText").slideToggle();
+  });
+
+  $(".att").click(function() {
+
+    $("#attTitle").slideToggle();
+    $("#attText").slideToggle();
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
